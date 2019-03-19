18fdf9db22f0af1d35ad8dab7895329a8607de38
Isaac <Isaac.est.cbtis@gmail.com>
2019-03-19 11:04:04 -0600

    agregar evento para clipboard jtable

---
+++ src/pruebas/Main.java
@@ -0,0 +1,17 @@
+package pruebas;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		String a = "30";
+
+		String b = "3";
+
+		System.out.println(a.compareTo(b));
+	}
+
+}
